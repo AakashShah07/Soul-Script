891606f43019f2c4b78e2550c98d1e029d918a37
AakashShah07 <angelayusender@gmail.com>
2025-02-21 12:39:50 +0530

    Adding id in URL when click the category button

--- components/categories.tsx
+++ components/categories.tsx
@@ -2,17 +2,46 @@
 
 import { cn } from "@/lib/utils";
 import { Category } from "@prisma/client";
+import { useSearchParams,useRouter } from "next/navigation";
 
+import qs from "query-string"
 interface CategoriesProps {
   data: Category[];
 }
 
 const Categories = ({ data }: CategoriesProps) => {
+
+    const router = useRouter();
+    const searchParams = useSearchParams();
+
+    const categoryId = searchParams.get("categoryId");
+
+    const onClick = (id: string | undefined) =>{
+        const query = {categoryId: id};
+        
+        const url = qs.stringifyUrl({
+            url: window.location.href,
+            query,
+        }, {skipNull: true});
+        router.push(url);
+
+    }
+
   return (
     <div className="w-full overflow-x-auto space-x-2 flex p-1">
+         <button
+         onClick={()=>onClick(undefined)}
+          className={cn(
+            "flex items-center text-center text-xs md:text-start px-2 md:px-4 py-2 md:py-2 rounded-md bg-primary/10 hover:opacity-75 transition"
+          )}
+        >
+            Newest
+        </button>
       {data.map((item) => (
         <button
           key={item.id}
+          onClick={()=>onClick(item.id)}
+
           className={cn(
             "flex items-center text-center text-xs md:text-start px-2 md:px-4 py-2 md:py-2 rounded-md bg-primary/10 hover:opacity-75 transition"
           )}
