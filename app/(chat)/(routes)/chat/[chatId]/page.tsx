e99f2100c59c1e90df0a1dc71fcbc9ce00cdbe48
AakashShah07 <angelayusender@gmail.com>
2025-05-02 00:21:45 +0530

    Changes the prop type in ChatIDPage

--- app/(chat)/(routes)/chat/[chatId]/page.tsx
+++ app/(chat)/(routes)/chat/[chatId]/page.tsx
@@ -1,53 +1,49 @@
-import prismadb from "@/lib/prismadb"
-import { redirect } from "next/navigation"
-import ChatClient from "./components/client"
-import { auth } from "@clerk/nextjs/server"
+import prismadb from "@/lib/prismadb";
+import { redirect } from "next/navigation";
+import ChatClient from "./components/client";
+import { auth } from "@clerk/nextjs/server";
 
-interface ChatIdProps{
-    params:{
-        chatId: string
-    }
+interface ChatIdProps {
+  params: {
+    chatId: string;
+  };
 }
 
-const ChatIdPage = async ({
-    params
-}: ChatIdProps) => {
+const ChatIdPage = async ({ params }) => {
+  const { userId } = auth();
 
-    const {userId} = auth()
+  const { chatId } = params;
 
-    
-    const {chatId} =  params;
-
-    const companion = await prismadb.companion.findUnique({
-        where:{
-            id: chatId
+  const companion = await prismadb.companion.findUnique({
+    where: {
+      id: chatId,
+    },
+    include: {
+      messages: {
+        orderBy: {
+          createdAt: "asc",
         },
-        include:{
-            messages:{
-                orderBy:{
-                    createdAt: "asc"
-                },
-                where:{
-                    userId
-                }
-            },
-            _count:{
-                select:{
-                    messages: true
-                }
-            }
-        }
-    });
+        where: {
+          userId,
+        },
+      },
+      _count: {
+        select: {
+          messages: true,
+        },
+      },
+    },
+  });
 
-    if(!companion){
-        return redirect("/");
-    }
+  if (!companion) {
+    return redirect("/");
+  }
 
   return (
     <div>
-      <ChatClient companion={companion}/>
+      <ChatClient companion={companion} />
     </div>
-  )
-}
+  );
+};
 
-export default ChatIdPage
+export default ChatIdPage;
