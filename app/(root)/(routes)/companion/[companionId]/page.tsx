01796020732fddaf92f828e166264ba2dcf05821
AakashShah07 <angelayusender@gmail.com>
2025-05-03 10:27:39 +0530

    New fix

--- app/(root)/(routes)/companion/[companionId]/page.tsx
+++ app/(root)/(routes)/companion/[companionId]/page.tsx
@@ -4,11 +4,6 @@ import { currentUser } from "@clerk/nextjs/server";
 import { redirect } from "next/dist/server/api-utils";
 import { RedirectToSignIn } from "@clerk/nextjs";
 
-interface CampanionIdPageProps  {
-    params: {
-        companionId: string
-    };
-};
 
 
 const CampanionIdPage = async({
